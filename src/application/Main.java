cff1c7889f548a426d2090b795abac7065b401d6
southPaw <dangargiullo@gmail.com>
2022-04-02 17:29:19 -0400

    Move 'Persistent' to 'out/'

--- src/application/Main.java
+++ src/application/Main.java
@@ -60,8 +60,8 @@ public class Main extends Application
 	@Override
 	public void init() {
 		try {
-			userBag = (UserBag) Utilities.load("Persistent/userbag");
-			library = (BookBag) Utilities.load("Persistent/library");
+			userBag = (UserBag) Utilities.load("out/Persistent/userbag");
+			library = (BookBag) Utilities.load("out/Persistent/library");
 			if (!userBag.containsUsername("Jane"))
 				userBag.insert(new Admin("Jane", "Doe", new ContactInfo(new Name("Jane", "Doe"), "N/A", "N/A", "N/A", "N/A", "N/A")));
 			if (!userBag.containsUsername("John"))
@@ -110,8 +110,8 @@ public class Main extends Application
 	@Override
 	public void stop() {
 		try {
-			Utilities.save(userBag, "Persistent/userbag");
-			Utilities.save(library, "Persistent/library");
+			Utilities.save(userBag, "out/Persistent/userbag");
+			Utilities.save(library, "out/Persistent/library");
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
